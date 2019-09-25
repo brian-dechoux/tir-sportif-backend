e922152c9868d6fe578abfea017367b149ab2df7
Brian Dechoux <brian.dechoux@intech.lu>
2019-09-25 10:42:29 +0200

    Init authorities and admin user with Docker

---
+++ docker/mysql/3_user.sql
@@ -0,0 +1,1 @@
+INSERT INTO `user` (email,username,password,authorityId) VALUES ('brian.dechoux.work@gmail.com','brian','$shiro1$SHA-256$500000$SxXnfkKA4WL5nZUG1oAa8A==$Iqx6akiftFkz6TA9AJtPIvmn3+tNp/ojtf6nDpZuLM8=',1);
