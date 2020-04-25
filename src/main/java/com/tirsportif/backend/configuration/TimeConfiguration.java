553123c9611489434477671dbebab186b37858b5
Brian Dechoux <brian.dechoux@intech.lu>
2020-04-25 15:03:40 +0200

    Clock with GMT Paris

--- src/main/java/com/tirsportif/backend/configuration/TimeConfiguration.java
+++ src/main/java/com/tirsportif/backend/configuration/TimeConfiguration.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Clock;
+import java.time.ZoneId;
 
 @Configuration
 public class TimeConfiguration {
 
     @Bean
     public Clock clock() {
-        return Clock.systemUTC();
+        return Clock.system(ZoneId.of("Europe/Paris"));
     }
 
 }
