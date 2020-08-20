1abdbedf98518b107afe0117d19a60a11a105bf5
Brian Dechoux <brian.dechoux@intech.lu>
2020-08-20 16:39:40 +0200

    Corrected bill search query

--- src/main/java/com/tirsportif/backend/repository/BillRepository.java
+++ src/main/java/com/tirsportif/backend/repository/BillRepository.java
@@ -12,27 +12,8 @@ import java.util.List;
 
 public interface BillRepository extends JpaRepository<Bill, Long> {
 
-    @Query(value = "SELECT * FROM bill " +
-            "INNER JOIN participation ON bill.participationId = participation.id " +
-            "WHERE participation.challengeId = ?1"
-            , nativeQuery = true)
-    List<Bill> getBillsForChallenge(Long challengeId);
-
-    @Query(value = "SELECT s.id, s.lastname, s.firstname FROM shooter s " +
-            "WHERE id IN (" +
-            "    SELECT DISTINCT CONCAT(COALESCE(l.shooterId, ''), COALESCE(p.shooterId, '')) AS shooterId " +
-            "    FROM bill b " +
-            "    LEFT JOIN participation p ON b.participationId = p.id " +
-            "    LEFT JOIN licensee l ON b.licenseeId = l.id" +
-            ") " +
-            "ORDER BY lastname, firstname"
-            , countQuery = "SELECT COUNT(DISTINCT CONCAT(COALESCE(l.shooterId,''), COALESCE(p.shooterId,''))) FROM bill b " +
-            "LEFT JOIN participation p ON b.participationId = p.id " +
-            "LEFT JOIN licensee l ON b.licenseeId = l.id"
-            , nativeQuery = true)
-    Page<ShooterWithBillsListElementProjection> findShootersWithBillsAsListElements(Pageable page);
-
-    @Query(value = "SELECT b.id, b.value, b.paid, b.paidDate, pr.type, c.name AS challengeName, c.startDate AS challengeStartDate, l.subscriptionDate AS licenseSubscriptionDate FROM bill b " +
+    @Query(value =
+            "SELECT b.id, b.value, b.paid, b.paidDate, pr.type, c.name AS challengeName, c.startDate AS challengeStartDate, l.subscriptionDate AS licenseSubscriptionDate FROM bill b " +
             "INNER JOIN price pr ON b.priceId = pr.id " +
             "LEFT JOIN participation p ON b.participationId = p.id " +
             "LEFT JOIN challenge c ON p.challengeId = c.id " +
@@ -41,5 +22,29 @@ public interface BillRepository extends JpaRepository<Bill, Long> {
             "OR l.shooterId = ?1)"
             , nativeQuery = true)
     List<ShooterBillProjection> getBillsForShooter(Long shooterId);
+
+    @Query(value =
+            "SELECT s.id, s.lastname, s.firstname FROM shooter s " +
+            "WHERE s.id IN (" +
+            "    (SELECT p.shooterId " +
+            "    FROM bill b " +
+            "    INNER JOIN participation p ON b.participationId = p.id), " +
+            "    (SELECT l.shooterId " +
+            "    FROM bill b " +
+            "    INNER JOIN licensee l ON b.licenseeId = l.id) " +
+            ") " +
+            "ORDER BY lastname, firstname"
+            , countQuery =
+            "SELECT COUNT(*) FROM shooter s " +
+            "WHERE s.id IN (" +
+            "     (SELECT p.shooterId " +
+            "     FROM bill b " +
+            "     INNER JOIN participation p ON b.participationId = p.id), " +
+            "     (SELECT l.shooterId " +
+            "     FROM bill b " +
+            "     INNER JOIN licensee l ON b.licenseeId = l.id)" +
+            ")"
+            , nativeQuery = true)
+    Page<ShooterWithBillsListElementProjection> findShootersWithBillsAsListElements(Pageable page);
 
 }
